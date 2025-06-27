3b3d93c8c3c15b2f47ddd118aa98928bf36b0ca5
Debora Campos <deboraccamposs@gmail.com>
2025-06-27 02:28:57 -0300

    Atualizações e melhorias no ContaTerminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new java.util.Scanner(System.in);
+        System.out.println("Bem-vindo ao Banco Java da DIO!\n Por favor, insira o número do seu banco:");
+        int number = scanner.nextInt();
+        scanner.nextLine(); // Consumir a quebra de linha pendente
+        System.out.println("Em seguida, informe sua agência: ");
+        String agency = scanner.nextLine();
+        System.out.println("Insira seu nome completo: ");
+        String fullname = scanner.nextLine();
+        System.out.println("Digite seu saldo: ");
+        Double balance = scanner.nextDouble();
+        System.out.println("Obrigado por fornecer suas informações!");
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco!\n" +
+                          "Número da conta: %d\n" +
+                          "Agência: %s\n" +
+                          "Seu saldo é de: R$ %.2f\nE já encontra-se disponível para saque!\n", fullname, number, agency, balance);
+        System.out.println("Aproveite nossos serviços!");
+        scanner.close();
     }
 }
-
-
-//git config --global user.email "you@example.com"
-  //git config --global user.name "Your Name"
