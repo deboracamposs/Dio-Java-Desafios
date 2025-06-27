84395052ae2d1ff1291c5a6625b33dde0705a23c
Debora Campos <deboraccamposs@gmail.com>
2025-06-27 01:51:58 -0300

    Projeto - Conta Banco

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,9 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+
+
+//git config --global user.email "you@example.com"
+  //git config --global user.name "Your Name"
